3e6781d3b4a49cd9d552f8be0c965b89aca8a6be
SouvagyaDash29 <souvagyadash1999@gmail.com>
2023-11-13 11:46:42 +0530

    Add to cart product

---
+++ src/main/java/com/example/shoppingcart/ShoppincartApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppincartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppincartApplication.class, args);
+    }
+
+}
